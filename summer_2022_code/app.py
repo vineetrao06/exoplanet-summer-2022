836c3cc22dc8fcc4883d0a9ebf04721a3067e2e0
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2022-07-16 15:36:20 -0700

    added the checking of controv flags

--- summer_2022_code/app.py
+++ summer_2022_code/app.py
@@ -1,1 +1,42 @@
-import elimination
+# @author: Vineet Rao
+
+import pandas as pd
+
+class Dataset:
+    def __init__(self, src="dataset.csv", newSrc="Exoplanet-Elimination.csv"):
+        self.src = src
+        self.df = pd.read_csv(src, index_col=False)
+        self.newSrc = newSrc
+
+
+    def dedupe(self):
+        # Sorts all the values in the dataset by discovery year
+        self.df.sort_values('disc_pubdate', ascending=True)
+
+        # Removing all the duplicates with the same name and same date
+        self.df = self.df.drop_duplicates(subset=["pl_name"], keep="last")
+
+        # for i in self.df.iterrows():
+        #     print(i[1])
+    
+    
+    # TODO: check all controversial flags, and remove entries w/ 1
+    # TODO: NOT YET FULLY TESTED 
+    def remove_controv_flags_of_1(self):
+        self.df = self.df[self.df['pl_controv_flag'] != '1']
+
+    def check_for_controv_flags_of_1(self):
+
+
+    # TODO: used supervised learning w/ control planet of k218b and k23d
+    def keplarian_mechanics(self):
+        pass
+
+    def export_csv(self):
+        self.df.to_csv('Exoplanet-Elimination.csv')
+
+if __name__ == "__main__":
+    newDataset = Dataset(src='dataset.csv')
+    newDataset.dedupe()
+    newDataset.remove_controv_flags_of_1()
+    newDataset.export_csv()
---
+++ summer_2022_code/check_controversial_flags.py
@@ -0,0 +1,66 @@
+# '''import csv
+
+# filename = 'dataset.csv'
+
+# rows_to_remove = list()
+
+
+with open(filename, 'r') as csvfile:
+    df = csv.reader(csvfile)
+    # print(df)
+
+    # Getting a list of all the rows to remove
+    for row in df:
+        pl_name = row[1]
+        controv_flag = row[32]
+        # print(row)
+        if (controv_flag == "1"):
+            print(pl_name + " has controversial flag of 1")
+            # Getting the row index value
+            rows_to_remove.append(row[0] - 1)
+
+
+
+# '''
+
+# import csv
+
+# lines = list()
+# with open('dataset.csv', 'r') as readFile:
+#     reader = csv.reader(readFile)
+#     for row in reader:
+#         lines.append(row)
+#         if row[32] == "1":
+#             lines.remove(row)
+
+# with open('without-controv-flags.csv', 'w') as writeFile:
+#     writer = csv.writer(writeFile)
+#     writer.writerows(lines)
+
+# with open('without-controv-flags.csv', 'w') as input, open('without-controv-flags-2.csv', 'w', newline='') as output:
+#      writer = csv.writer(output)
+#      for row in csv.reader(input):
+#          if any(field.strip() for field in row):
+#              writer.writerow(row)
+
+
+
+# rows_to_remove = list()
+
+# with open('without-controv-flags.csv', 'r') as csvfile:
+#     df = csv.reader(csvfile)
+#     # print(df)
+
+#     # Getting a list of all the rows to remove
+#     for row in df:
+#         pl_name = row[1]
+#         controv_flag = row[32]
+#         # print(row)
+#         if (controv_flag == "1"):
+#             print(pl_name + " has controversial flag of 1")
+#             # Getting the row index value
+#             rows_to_remove.append(int(row[0]) - 1)
+
+
+
+df = df[df['Team'] != 'C']
